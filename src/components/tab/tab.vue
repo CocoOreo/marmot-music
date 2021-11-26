f76d33f6264d212ee62d0c14604771c24cd0d3df
CocoOreo <songhaoranuestc@163.com>
2021-11-26 20:22:06 +0800

    feat(tab): Adding the tab and initialize the router.

---
+++ src/components/tab/tab.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="tab">
+    <router-link
+      class="tab-item"
+      v-for="tab in tabs"
+      :key="tab.path"
+      :to="tab.path"
+    >
+      <span class="tab-link">
+        {{tab.name}}
+      </span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tab',
+  data () {
+    return {
+      tabs: [
+        {
+          name: 'Recommend',
+          path: '/recommend'
+        },
+        {
+          name: 'Singer',
+          path: '/singer'
+        },
+        {
+          name: 'Top',
+          path: '/top-list'
+        },
+        {
+          name: 'Search',
+          path: '/search'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .tab {
+    display: flex;
+    height: 40px;
+    line-height: 40px;
+    font-size: $font-size-medium;
+    background-color: darken($color-background, 6%);
+    .tab-item {
+      flex: 1;
+      text-align: center;
+
+      .tab-link {
+        padding-bottom: 5px;
+        color: $color-text-l;
+      }
+
+      &.router-link-active {
+        .tab-link {
+          color: $color-theme;
+          border-bottom: 2px solid $color-theme;
+        }
+      }
+    }
+  }
+</style>
