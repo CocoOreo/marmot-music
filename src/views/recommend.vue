2f4f7bc95419c68f1813a74725c922849106becc
CocoOreo <songhaoranuestc@163.com>
2021-11-29 17:42:58 +0800

    feat(slide): Adding the slide component.

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,23 +1,47 @@
 <template>
-<div class="recommend">
-    recommend
-</div>
+  <div class="recommend">
+    <div class="slider-container">
+      <div class="slider-content">
+        <slider v-if="sliders.length" :sliders="sliders"></slider>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { getRecommend } from '@/service/recommend'
+import Slider from '@/components/slider/slider'
 export default {
   name: 'recommend',
   data () {
-
+    return {
+      sliders: []
+    }
   },
   async created () {
     const result = await getRecommend()
-    console.log(result)
+    const { sliders } = result
+    this.sliders = sliders
+  },
+  components: {
+    Slider
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.slider-container {
+  position: relative;
+  widows: 100%;
+  height: 0;
+  padding-top: 40%;
+  overflow: hidden;
+  .slider-content {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+  }
+}
 </style>
